014045815572be01cba2da9334efcecd050971df
Haroon_R9 <haroonrind8@gmail.com>
2024-04-25 00:38:37 +0500

    nested

---
+++ nested-for-loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int k=1; k<10; k++)
+    {
+    for(int i=1; i<=k; i++)
+    
+        cout<<i<<"\t";
+        cout<<endl;
+    }
+    return 0;
+}

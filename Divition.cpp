634020a16d911dfad5f56e53f1ef6162c494a654
Haroon_R9 <haroonrind8@gmail.com>
2024-04-25 22:12:17 +0500

    Arithematic operation

---
+++ Divition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    
+    cout << "Enter num1 :" << endl;
+    cin >> num1;
+
+    cout << "Enter num2 :"<< endl;
+    cin >> num2;
+
+    cout << "The divition is :"<< num1 / num2;
+
+    return 0;
+}
